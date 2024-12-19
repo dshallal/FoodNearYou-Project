a7c48ea02d9c0e06f21e6d8d1ffbead0109d360a
Dan Shallal <dshallal11@gmail.com>
2024-12-19 00:46:56 -0500

    Major changes

--- pup2.js
+++ pup2.js
@@ -1,31 +1,28 @@
-const puppeteer = require("puppeteer");
+puppeteer = require("puppeteer");
 
 (async () => {
     const express = require('express');
-    const cors = require('cors');
-    
+    const cors = require('cors'); 
     const app = express();
-    app.listen(3010, () => {
+    app.listen(3011, () => {
         console.log(`Server is running on port 3010.`);
       });
-    const browser = await puppeteer.launch({headless: false});
+     const browser = await puppeteer.launch({headless: false});
     const context = await browser.createBrowserContext();
     await context.overridePermissions("https://google.com/", ["geolocation"]);
     const page = await context.newPage();
     await page.goto("https://www.google.com/", {waitUntil: "domcontentloaded"});
-    await page.waitForSelector('#APjFqb', {visible: true});
-    await page.type('#APjFqb', "Fast Food Near Me");
+   // await page.waitForSelector('#APjFqb', {visible: true});
+    await page.type('#APjFqb', "Dine In Restaurants Near Me");
     await page.keyboard.press("Enter");
     await page.waitForNavigation();
-    //await page.click("#Odp5De > div > div > div.ixix9e > div:nth-child(2) > div.av9nEd > div > div.kuydt > div:nth-child(2) > div > h3 > g-more-link > a");
-    const link = await page.evaluate(()=> {
+    const link3 = await page.evaluate(()=> {
         const link2 =  document.querySelector("#Odp5De > div:nth-child(1) > div > div > div > div > div > div.ixix9e > div.av9nEd > div > div.kuydt > div:nth-child(2) > div > h3 > g-more-link > a");
         const link_src = link2.getAttribute("href");
         return link_src;
     });
-    await page.goto("https://google.com/" + link);
-    await page.screenshot({path: "web.png"});
-    const details = await page.evaluate(() =>{
+    await page.goto("https://google.com/" + link3);
+    const details3 = await page.evaluate(() =>{
         const image = document.querySelectorAll(".rllt__details");
         let quotesArr = [];
         image.forEach((tag) => {
@@ -34,18 +31,18 @@ const puppeteer = require("puppeteer");
 
         return quotesArr;
     });
-    console.log(details);
-    var quotesArr2 = [];
-    for(let i  = 1; i < details.length; i++){
-        let indexes = details[i].split("\n");
-        quotesArr2.push(indexes[0]);
+    console.log(details3);
+    var quotesArr10 = [];
+    for(let i  = 1; i < details3.length; i++){
+        let indexes = details3[i].split("\n");
+        quotesArr10.push(indexes[0]);
     }
-    console.log(quotesArr2);
-    var quotesArr3 = [];
-    for(let i = 0; i < quotesArr2.length; i++){
+    console.log(quotesArr10);
+    var quotesArr11 = [];
+    for(let i = 0; i < quotesArr10.length; i++){
         await page.goto("https://images.google.com/", {waitUntil: "domcontentloaded"});
         await page.waitForSelector('#APjFqb', {visible: true});
-        await page.type('#APjFqb', quotesArr2[i]);
+        await page.type('#APjFqb', quotesArr10[i] + "Restaurant");
         await page.keyboard.press("Enter");
         await page.waitForNavigation();
         //await page.waitForNavigation();
@@ -60,23 +57,18 @@ const puppeteer = require("puppeteer");
                 arr3 = "";
             }
         });
-        quotesArr3.push(images4);
-        console.log(quotesArr3);
+        quotesArr11.push(images4);
+        console.log(quotesArr11);
     }
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/details1', (req, res) => {
-    res.json({ "details1": details});
-});
-
-app.get("/details2", (req, res) => {
-    res.json({"details2": quotesArr3});
-})
-    
-    await browser.close();
-
-
-
+        
+        app.use(cors());
+        app.use(express.json());
+        app.get("/details3", (req, res) => {
+            res.json({"details3": details3});
+        })
+        
+        app.get("/details4", (req, res) => {
+            res.json({"details4": quotesArr11});
+        })
+          await browser.close();
 })();
